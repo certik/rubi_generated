79613660c77db95736b1d4eb6fbfb29081230364
Ondřej Čertík <ondrej@certik.us>
2020-12-15 11:20:29 -0700

    Update

--- run_example.py
+++ run_example.py
@@ -6,19 +6,19 @@ from utils import check_constraints
 
 expr_result = [
     (Integral(x, x), x**2/2),
-    #(Integral(x**2, x), x**3/3),
-    #(Integral(x**3, x), x**4/4),
+    (Integral(x**2, x), x**3/3),
+    (Integral(x**3, x), x**4/4),
     #(Integral((x + 1)/(1 - x), x), -x - 2*log(x - 1)),
     #(Integral(1/(x + 1), x), log(x + 1)),
     #(Integral(x/(x**2 + 1), x), log(x**2 + 1)/2),
-    #(Integral(1/x, x), log(x)),
+    (Integral(1/x, x), log(x)),
     #(Integral(sin(x), x), -cos(x)),
     #(Integral(sin(x)*cos(x), x), sin(x)**2/2),
     #(Integral(exp(x**2)/2, x), sqrt(pi)*erfi(x)/4),
     #(Integral(log(x), x), x*log(x) - x),
-    #(Integral(x**a, x), x**(a + S(1))/(a + S(1))),
+    (Integral(x**a, x), x**(a + S(1))/(a + S(1))),
     #(Integral(S(1)/x, x), log(x)),
-    #(Integral(a*x, x), a*(S(1)/S(2))*x**S(2)),
+    (Integral(a*x, x), a*(S(1)/S(2))*x**S(2)),
     #(Integral(1/(x**2*(a + b*x)**2), x), -b/(a**2*(a + b*x)) - 1/(a**2*x) - 2*b*log(x)/a**3 + 2*b*log(a + b*x)/a**3),
     #(Integral(x**6/(a + b*x)**2, x), (-a**6/(b**7*(a + b*x)) - S(6)*a**5*log(a + b*x)/b**7 + 5*a**4*x/b**6 - S(2)*a**3*x**2/b**5 + a**2*x**3/b**4 - a*x**4/(S(2)*b**3) + x**5/(S(5)*b**2))),
     #(Integral(1/(x**2*(a + b*x)**2), x), -b/(a**2*(a + b*x)) - 1/(a**2*x) - 2*b*log(x)/a**3 + 2*b*log(a + b*x)/a**3),
@@ -84,7 +84,8 @@ for expr, expected in expr_result:
     results = [repl(**subst) for repl, subst, constraints in ret if check_constraints(subst, constraints)]
     #results = [rubi_decision_integrator(expr)]
 
-    clean = [i for i in results if not isinstance(i, Function)]
+    #clean = [i for i in results if not isinstance(i, Function)]
+    clean = results
     pprint(expr)
     pprint(clean)
     print("==============================")
